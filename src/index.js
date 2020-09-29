de6b36b52a27dd4ecfe812aa7f7866b26a4d57a2
100Rubley <toxa_98@inbox.ru>
2020-09-29 15:46:07 +0300

    feat: test pass

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let result = 0
+  if (arguments.length != 0) {
+    if (array.length != 0) {
+      result = array.sort((a, b) => a - b)[0]
+    }
+  }
+  return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let result = 0
+  if (arguments.length != 0) {
+    if (array.length != 0) {
+      result = array.sort((a, b) => b - a)[0]
+    }
+  }
+  return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let result = 0
+  let sum = 0
+  if (arguments.length != 0) {
+    if (array.length != 0) {
+      for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+        result = sum / array.length
+      }
+    }
+  }
+  return result;
 }
+
